6e5a280552b3ce75c230d2af7135e3ce6d0b486e
Plumbio <felipe.b@live.cl>
2021-06-11 20:26:44 -0400

    Hecho el código del movimiento del escenario y fondo infinito, avances con geyobject de Player y avances con su código

--- GMTKGameJam2021/Assets/Scripts/Mg_ScenarioMovement.cs
+++ GMTKGameJam2021/Assets/Scripts/Mg_ScenarioMovement.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mg_ScenarioMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Mg_ScenarioMovement : MonoBehaviour {
+
+    float scenarioSpeed = 2;
+    public bool move = true;
+
+    public List<Transform> backgroundPaperTransform;
+    Transform currentManagedPaperTransform;
+
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update () {
+        if (!move)
+            return;
+        transform.Translate(0, -scenarioSpeed * Time.deltaTime, 0);
+    }
+
+    void FixedUpdate() {
+        if(backgroundPaperTransform[0].position.y <= -9) {
+            currentManagedPaperTransform = backgroundPaperTransform[0];
+            backgroundPaperTransform.RemoveAt(0);
+            currentManagedPaperTransform.Translate(0, 18.75f, 0);
+            backgroundPaperTransform.Add(currentManagedPaperTransform);
+        }
     }
 }
--- GMTKGameJam2021/Assets/Scripts/Pj_CharController.cs
+++ GMTKGameJam2021/Assets/Scripts/Pj_CharController.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pj_CharController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Pj_CharController : MonoBehaviour {
+
+    float playerSpeed = 5;
+
+    string playerController = "0";
+    float lastHorValue;
+
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetButtonDown("ButtonA" + playerController)){
+            Jump();
+        }
+        if (Input.GetAxis("Horizontal" + playerController) != lastHorValue){
+            Move(Input.GetAxis("Horizontal" + playerController));
+        }
+    }
+
+    void Jump () {
+
+    }
+    void Move (float value) {
     }
 }
